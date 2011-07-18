08a3fb59520ac756c97b75ece47ab24e448e3b76
eltonkent <eltonkent@gmail.com>
2011-07-18 11:41:27 +0000

    added copyright

--- src/com/madrobot/db/auto/BeanGenerator.java
+++ src/com/madrobot/db/auto/BeanGenerator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 MadRobot.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *  Elton Kent - initial API and implementation
+ ******************************************************************************/
 package com.madrobot.db.auto;
 
 import java.lang.reflect.InvocationTargetException;
@@ -21,6 +31,11 @@ import com.madrobot.beans.PropertyDescriptor;
 import com.madrobot.db.DBUtils;
 import com.madrobot.reflect.ClassUtils;
 
+/**
+ * Utility class to map a database cursor/resultset to a java bean
+ * @author elton.kent
+ *
+ */
 public class BeanGenerator {
 	/**
 	 * Set a bean's primitive properties to these defaults when SQL NULL is
