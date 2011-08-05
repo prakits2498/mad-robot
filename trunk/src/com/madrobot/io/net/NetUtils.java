8e88eb1eab7e9962ce53b7613862f1453b0238d9
eltonkent@gmail.com <eltonkent@gmail.com@0e9ddb6a-d1f9-9a91-840c-fda4013f761d>
2011-08-05 07:10:57 +0000

    added net utils

--- trunk/src/com/madrobot/io/net/NetUtils.java
+++ trunk/src/com/madrobot/io/net/NetUtils.java
@@ -29,26 +29,24 @@ public class NetUtils {
 	 * @return The first non-loopback IPv4 address found, or <code>null</code>
 	 *         if no such addresses found
 	 * @throws SocketException
-	 *             If there was a problem querying the network
-	 *             interfaces
+	 *             If there was a problem querying the network interfaces
 	 */
 	public static InetAddress getLocalAddress() throws SocketException {
-		Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
-		while(ifaces.hasMoreElements()){
+		Enumeration<NetworkInterface> ifaces = NetworkInterface
+				.getNetworkInterfaces();
+		while (ifaces.hasMoreElements()) {
 			NetworkInterface iface = ifaces.nextElement();
 			Enumeration<InetAddress> addresses = iface.getInetAddresses();
 
-			while(addresses.hasMoreElements()){
+			while (addresses.hasMoreElements()) {
 				InetAddress addr = addresses.nextElement();
-				if((addr instanceof Inet4Address) && !addr.isLoopbackAddress()){
+				if ((addr instanceof Inet4Address) && !addr.isLoopbackAddress()) {
 					return addr;
 				}
 			}
 		}
 		return null;
 	}
-	
-	
 
 	/**
 	 * Finds this computer's global IP address
@@ -58,13 +56,14 @@ public class NetUtils {
 	 * @return The global IP address, or null if a problem occurred
 	 */
 	public static Inet4Address getGlobalAddress(String url) {
-		try{
+		try {
 			URLConnection uc = new URL(url).openConnection();
-			BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					uc.getInputStream()));
 			return (Inet4Address) InetAddress.getByName(br.readLine());
-		} catch(MalformedURLException e){
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
-		} catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
@@ -104,4 +103,129 @@ public class NetUtils {
 		return i;
 	}
 
+	/**
+	 * Validate the given IPv4 address.
+	 * 
+	 * @param address
+	 *            the IP address as a String.
+	 * 
+	 * @return true if a valid IPv4 address, false otherwise
+	 */
+	public static boolean isValidIPv4(String address) {
+		if (address.length() == 0) {
+			return false;
+		}
+
+		int octet;
+		int octets = 0;
+
+		String temp = address + ".";
+
+		int pos;
+		int start = 0;
+		while (start < temp.length()
+				&& (pos = temp.indexOf('.', start)) > start) {
+			if (octets == 4) {
+				return false;
+			}
+			try {
+				octet = Integer.parseInt(temp.substring(start, pos));
+			} catch (NumberFormatException ex) {
+				return false;
+			}
+			if (octet < 0 || octet > 255) {
+				return false;
+			}
+			start = pos + 1;
+			octets++;
+		}
+
+		return octets == 4;
+	}
+
+	public static boolean isValidIPv4WithNetmask(String address) {
+		int index = address.indexOf("/");
+		String mask = address.substring(index + 1);
+
+		return (index > 0) && isValidIPv4(address.substring(0, index))
+				&& (isValidIPv4(mask) || isMaskValue(mask, 32));
+	}
+
+	public static boolean isValidIPv6WithNetmask(String address) {
+		int index = address.indexOf("/");
+		String mask = address.substring(index + 1);
+
+		return (index > 0)
+				&& (isValidIPv6(address.substring(0, index)) && (isValidIPv6(mask) || isMaskValue(
+						mask, 128)));
+	}
+
+	/**
+	 * Validate the given IPv6 address.
+	 * 
+	 * @param address
+	 *            the IP address as a String.
+	 * 
+	 * @return true if a valid IPv4 address, false otherwise
+	 */
+	public static boolean isValidIPv6(String address) {
+		if (address.length() == 0) {
+			return false;
+		}
+
+		int octet;
+		int octets = 0;
+
+		String temp = address + ":";
+		boolean doubleColonFound = false;
+		int pos;
+		int start = 0;
+		while (start < temp.length()
+				&& (pos = temp.indexOf(':', start)) >= start) {
+			if (octets == 8) {
+				return false;
+			}
+
+			if (start != pos) {
+				String value = temp.substring(start, pos);
+
+				if (pos == (temp.length() - 1) && value.indexOf('.') > 0) {
+					if (!isValidIPv4(value)) {
+						return false;
+					}
+
+					octets++; // add an extra one as address covers 2 words.
+				} else {
+					try {
+						octet = Integer
+								.parseInt(temp.substring(start, pos), 16);
+					} catch (NumberFormatException ex) {
+						return false;
+					}
+					if (octet < 0 || octet > 0xffff) {
+						return false;
+					}
+				}
+			} else {
+				if (pos != 1 && pos != temp.length() - 1 && doubleColonFound) {
+					return false;
+				}
+				doubleColonFound = true;
+			}
+			start = pos + 1;
+			octets++;
+		}
+
+		return octets == 8 || doubleColonFound;
+	}
+
+	private static boolean isMaskValue(String component, int size) {
+		try {
+			int value = Integer.parseInt(component);
+
+			return value >= 0 && value <= size;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
 }
