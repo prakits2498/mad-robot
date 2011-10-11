c0e272dc31822464870118d6c2bbd45d4e8fd8bf
eltonkent <eltonkent@gmail.com>
2011-10-11 11:35:15 +0000

    Added app utils. http response caching for ice-cream sandwich

--- src/com/madrobot/io/net/HttpTaskHelper.java
+++ src/com/madrobot/io/net/HttpTaskHelper.java
@@ -33,6 +33,7 @@ import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HTTP;
 
+import android.net.http.AndroidHttpClient;
 import android.util.Log;
 
 /**
