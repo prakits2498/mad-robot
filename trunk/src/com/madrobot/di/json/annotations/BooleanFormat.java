0094a8055b8a070f61f4872e5ba2ebbf1b8c3633
eltonkent@gmail.com <eltonkent@gmail.com@0e9ddb6a-d1f9-9a91-840c-fda4013f761d>
2011-10-21 12:44:53 +0000

    added json serializer /deserializer

---
+++ trunk/src/com/madrobot/di/json/annotations/BooleanFormat.java
@@ -0,0 +1,36 @@
+
+package com.madrobot.di.json.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.madrobot.di.json.JSONDeserializer;
+import com.madrobot.di.json.JSONSerializer;
+
+/**
+ * Annotation to specify the boolean format in the json string. <br/>
+ * See {@link JSONDeserializer} {@link JSONSerializer} for usage
+ * 
+ * @see {@link JSONDeserializer}, {@link JSONSerializer}
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
+public @interface BooleanFormat {
+
+	/**
+	 * Represent the true format
+	 * 
+	 * @return true format
+	 */
+	String trueFormat();
+
+	/**
+	 * Represent the false format
+	 * 
+	 * @return false format , default false
+	 */
+	String falseFormat() default "false";
+}
